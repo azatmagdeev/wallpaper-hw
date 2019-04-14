8875034e959afbea932f427ed5d86b9563e37273
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-14 06:43:21 +0000

    Deploy azatmagdeev/wallpaper-hw to github.com/azatmagdeev/wallpaper-hw.git:gh-pages

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import {calculateRolls} from "./lib.js";
+
+const result = calculateRolls(6,5,2.75,1.06,10);
+console.log(result);
+
+
+const result2 = calculateRolls(4,3,2.75,1.06,10);
+console.log(result2);
+
+
+
+
+
